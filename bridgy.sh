6a57011f9c12c5e1e677478a534d82b1a95b9fb0
Nicolino Curalli <nicolino.curalli@gmail.com>
2019-02-09 21:21:23 +0100

    added wrapper script to simulate executable behaviour

---
+++ bridgy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+volume_ssh_ouside=${HOME}/.ssh
+volume_aws_outside=${HOME}/.aws
+volume_bridgy_outside=${HOME}/bridgy
+
+volume_ssh_inside="/home/bridgy/.ssh"
+volume_aws_inside="/home/bridgy/.aws"
+volume_bridgy_inside="/home/bridgy/.bridgy"
+
+
+
+if [ $# -eq 0 ]
+    then
+        base_command="docker run -it -v ${volume_ssh_ouside}:${volume_ssh_inside} \
+                -v ${volume_bridgy_outside}:${volume_bridgy_inside} \
+                -v ${volume_aws_outside}:${volume_aws_inside} \
+                quay.io/nicolinocuralli/bridgy"
+    else
+        base_command="docker run -it -v ${volume_ssh_ouside}:${volume_ssh_inside} \
+                -v ${volume_bridgy_outside}:${volume_bridgy_inside} \
+                -v ${volume_aws_outside}:${volume_aws_inside} \
+                quay.io/nicolinocuralli/bridgy bridgy"
+fi
+
+command="$base_command"" "
+command+="$@"
+eval "$command"
+printf "\n"
+
+
